5507854c7bd3ffc8dab8c871d0bce54ecd362067
김윤지 <21615008@konyang.ac.kr>
2024-06-04 18:47:20 +0900

    초기 커밋.

---
+++ 01b/StartPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01b
+{
+    /// <summary>
+    /// StartPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // START 버튼 클릭 시 동작 정의
+            MessageBox.Show("START 버튼 클릭됨");
+            StartPage newPage = new StartPage();
+            this.Content = newPage;
+        }
+
+        private void Chapter1Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Chapter 1 버튼 클릭 시 동작 정의
+            MessageBox.Show("Chapter 1 버튼 클릭됨");
+        }
+
+        private void Chapter2Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Chapter 2 버튼 클릭 시 동작 정의
+            MessageBox.Show("Chapter 2 버튼 클릭됨");
+        }
+
+        private void Chapter3Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Chapter 3 버튼 클릭 시 동작 정의
+            MessageBox.Show("Chapter 3 버튼 클릭됨");
+        }
+
+        private void Chapter4Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Chapter 4 버튼 클릭 시 동작 정의
+            MessageBox.Show("Chapter 4 버튼 클릭됨");
+        }
+    }
+}
